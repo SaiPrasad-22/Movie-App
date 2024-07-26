c54c78521a204f94d6688fcabcbaa65b53a86603
saiprasad-22 <saisarikonda22@gmail.com>
2024-07-27 00:36:30 +0530

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let genre = document.getElementById("genre");
+let language = document.getElementById("language");
+let date = document.getElementById("date");
+let director = document.getElementById("director");
+let actors = document.getElementById("actors");
+let writer = document.getElementById("writer");
+let awards = document.getElementById("awards");
+let ratings = document.getElementById("ratings");
+let poster = document.getElementById("poster");
+
+// API
+let apiKey = "2fa0df07";
+let apiBaseUrl = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=";
+
+function searchMovie() {
+  let movieName = document.getElementById("movieName");
+  let url = apiBaseUrl + movieName.value;
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      console.log(data);
+      title.innerText = data.Title;
+      description.innerText = data.Plot;
+      genre.innerText = data.Genre;
+      language.innerText = data.Language;
+      date.innerText = data.Released;
+      director.innerText = data.Director;
+      actors.innerText = data.Actors;
+      writer.innerText = data.Writer;
+      awards.innerText = data.Awards;
+      ratings.innerText = data.imdbRating;
+      poster.src = data.Poster;
+    });
+}
